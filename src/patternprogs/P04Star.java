78d448a9f973ba2c69eaff4aeae0dd1b3727328b
Gaurav chauhan <gauravchauhan200@gmail.com>
2022-06-30 07:00:57 +0530

    Initial commit

---
+++ src/patternprogs/P04Star.java
@@ -0,0 +1,37 @@
+package patternprogs;
+
+public class P04Star 
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=5;i++)			//lines
+		{
+			
+			
+			for(int j=4;j>=i;j--)  		//space	
+			{		
+				System.out.print("-");
+			}
+			
+			
+			for(int k=1;k<=i;k++ )		//stars
+			{
+				if(i>=2 && k>1)		//2nd line and *>1 
+				{
+					System.out.print("-");
+				}
+			else
+				System.out.print("*");
+			}
+			
+			
+			
+			
+			
+			System.out.println();
+		}
+	}
+	
+}
+
+
---
+++ src/patternprogs/P05.java
@@ -0,0 +1,46 @@
+package patternprogs;
+
+/*
+
+----*
+---*-
+--*--
+-*---
+*----
+
+
+*/
+public class P05
+{
+	public static void main(String args[])
+	{
+	
+		for(int i=1;i<=5;i++)	//lines
+		{
+			for(int j=4;j>=i;j--)		//space triangle
+			{
+				System.out.print("-");
+			}
+			
+			
+			for(int k=1;k<=i;k++)	//star and space
+			{
+				if(i>=2 && k>1)
+				{
+					System.out.print("-");
+				}
+				else
+				{System.out.print("*");}
+			}
+			
+			
+			System.out.println();
+		}
+	
+	}
+	
+}
+
+
+
+
